70646657f41c4d7bbe6d7fdc1d1bf1715920de34
0v1se <dev@0v1se.com>
2018-02-12 10:54:43 +0100

    fix inheritance

--- contracts/SecuredImpl.sol
+++ contracts/SecuredImpl.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "./Secured.sol";
 import "./Ownable.sol";
 
-contract SecuredImpl is Secured, Ownable {
+contract SecuredImpl is Ownable, Secured {
 	mapping(string => address) users;
 	event RoleTransferred(address indexed previousUser, address indexed newUser, string role);
 
